d07e8fce9ab81c10be727badd0b9325977d56027
Ryo IIDA <ryoaksum@gmail.com>
2019-07-10 21:01:58 +0900

    add ora

--- main.go
+++ main.go
@@ -1,57 +1,64 @@
 package main
 
 import (
-  "bytes"
-  "log"
-  "net/http"
-  "os"
-  "strconv"
+	"bytes"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 
-  "github.com/gin-gonic/gin"
-  "github.com/russross/blackfriday"
-  _ "github.com/heroku/x/hmetrics/onload"
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/russross/blackfriday"
 )
 
 var (
-  repeat int
+	repeat int
 )
 
 func repeatHandler(c *gin.Context) {
-  var buffer bytes.Buffer
-  for i := 0; i < repeat; i++ {
-    buffer.WriteString("Hello from Go!\n")
-  }
-  c.String(http.StatusOK, buffer.String())
+	var buffer bytes.Buffer
+	for i := 0; i < repeat; i++ {
+		buffer.WriteString("Hello from Go!\n")
+	}
+	c.String(http.StatusOK, buffer.String())
+}
+
+func oraoraHandler(c *gin.Context) {
+	var buffer bytes.Buffer
+	buffer.WriteString("ORA ORA!\n")
+	c.String(http.StatusOK, buffer.String())
 }
 
 func main() {
-  var err error
-  port := os.Getenv("PORT")
+	var err error
+	port := os.Getenv("PORT")
 
-  if port == "" {
-    log.Fatal("$PORT must be set")
-  }
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
-  // .env に定義した値を利用する
-  tStr := os.Getenv("REPEAT")
-  repeat, err = strconv.Atoi(tStr)
-  if err != nil {
-    log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
-    repeat = 5
-  }
+	// .env に定義した値を利用する
+	tStr := os.Getenv("REPEAT")
+	repeat, err = strconv.Atoi(tStr)
+	if err != nil {
+		log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
+		repeat = 5
+	}
 
-  router := gin.New()
-  router.Use(gin.Logger())
-  router.LoadHTMLGlob("templates/*.tmpl.html")
-  router.Static("/static", "static")
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
 
-  router.GET("/", func(c *gin.Context) {
-    c.HTML(http.StatusOK, "index.tmpl.html", nil)
-  })
-  router.GET("/mark", func(c *gin.Context) {
-    c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
-  })
-  router.GET("/repeat", repeatHandler)
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+	router.GET("/mark", func(c *gin.Context) {
+		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+	})
+	router.GET("/repeat", repeatHandler)
+	router.GET("/oraora", oraoraHandler)
 
-  router.Run(":" + port)
+	router.Run(":" + port)
 }
